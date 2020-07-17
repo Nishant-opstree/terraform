642620e0df01327fd3b9f0edd598731e344e2cec
trickste <nishantprakashsaini285@gmail.com>
2020-07-17 22:38:00 +0530

    upload

---
+++ change_backend_status.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+infrastructure_branch="$1"
+backend_status=`cat backend_status | awk '/s3_backend_configured/{print $2}'`
+if [ backend_status == false ]
+then
+	terraform init
+	terraform apply -auto-approve
+	sudo rm terraform_backend_setup.tf
+	sed -i "/s3_backend_configured/s/false/true/" backend_status
+	backend_data="""terraform {\n
+    backend "s3"  {\n
+        bucket = "nishant-terraform-statebucket-test"\n
+	key            = "global/s3/terraform.tfstate"\n
+        region         = "ap-south-2"\n
+    }\n
+}\n
+	"""
+	echo -e "$backend_data" | cat - main.tf > temp && mv temp main.tf
+	rm terraform.*
+	git add .
+	git push origin test_master
+
+fi
+
+git merge "$infrastructure_branch"
---
+++ main.tf
@@ -0,0 +1,14 @@
+provider "aws" {
+  profile    = "default"
+  region     = "ap-south-1"
+}
+
+resource "aws_dynamodb_table" "terraform_locks" {
+  name         = "nishant_terraform_state_bucket_locks"
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key     = "LockID"  
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
+}
