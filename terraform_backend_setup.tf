996c27e7ef24c3b4a678dc2579af2ae36e61fddd
trickste <nishantprakashsaini285@gmail.com>
2020-07-18 09:23:38 +0530

    upload

--- terraform_backend_setup.tf
+++ terraform_backend_setup.tf
@@ -1,7 +1,3 @@
-provider "aws" {
-  profile    = "default"
-  region     = "ap-south-1"
-}
 resource "aws_s3_bucket" "terraform_state" {
   bucket = "nishant-terraform-state-bucket-test" 
   versioning {
